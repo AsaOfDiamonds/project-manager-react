02678e91204f7e90a76a30c6b2866b5ed3b7c8a7
1@$@1github <asaofdiamonds@gmail.com>
2018-11-21 10:07:06 -0700

    added form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Projects from "./Components/Projects";
-
+import AddProject from "./Components/AddProject";
 import './App.css';
 
 class App extends Component {
@@ -27,10 +27,16 @@ class App extends Component {
      }
    ]}); 
   }
+
+  handleAddProject(project){
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects:projects});
+  }
   render() {
     return (
       <div className="App">
-        My App
+        <AddProject AddProject={this.handleAddProject.bind(this)} />
         <Projects projects={this.state.projects} />
       </div>
     );

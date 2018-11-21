b965289e3f898dc719379c894af92a912748b487
1@$@1github <asaofdiamonds@gmail.com>
2018-11-20 17:50:46 -0700

    first commit

---
+++ src/Components/Projects.js
@@ -0,0 +1,14 @@
+import React, { Component  } from "react";
+
+class Projects extends Component {
+    render() {
+        return (
+            <div className='Projects'>
+            My projects
+            
+            </div>
+        );
+    }
+}
+
+export default Projects;
